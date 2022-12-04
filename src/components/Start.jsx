eed8155e89d3c7742e7f58c6fb84675b92ae196e
SOMYA RAWAT <100460439+codewithsom@users.noreply.github.com>
2022-12-04 12:15:05 +0530

    Add files via upload

---
+++ src/components/Start.jsx
@@ -0,0 +1,22 @@
+import { useRef } from "react";
+
+export default function Start({ setUsername }) {
+  const inputRef = useRef();
+
+  const handleClick = () => {
+    inputRef.current.value && setUsername(inputRef.current.value);
+  };
+
+  return (
+    <div className="start">
+      <input
+        className="startInput"
+        placeholder="Enter your name 🙂"
+        ref={inputRef}
+      />
+      <button className="startButton" onClick={handleClick}>
+        Start 🔥
+      </button>
+    </div>
+  );
+}
